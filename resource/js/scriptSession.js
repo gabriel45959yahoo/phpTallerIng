c60229b14a35c3cf4bc4386b580716cf20a1f238
Gabriel David Gimenez <gabriel45959@gmail.com>
2017-09-21 22:20:36 -0300

    Gabriel: corrección para el control de logueo.

--- resource/js/scriptSession.js
+++ resource/js/scriptSession.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-    $("#body").load("../control/SessionController.php");
-});
+function redireccionar() {
+    // $("#body").load("../control/SessionController.php");
+     $.ajax({
+         type: "POST",
+         url: "../control/SessionController.php",
+         data: {datos:"datos"},
+         success: function (data) {
+           //data es la respuesta del php
+           if(data == "error"){
+        	   window.location.assign('/index.html');
+           }
+       }
+ });
+}
