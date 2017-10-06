6216ff6212dff6e7321f00d79ce608980115f2da
Gabriel David Gimenez <gabriel45959@gmail.com>
2017-10-05 23:53:12 -0300

    Gabriel: cambios en apadrinaje

--- resource/js/scriptConsulta.js
+++ resource/js/scriptConsulta.js
@@ -47,8 +47,6 @@ function loadTablaPadrino(){
             success: function (data) {
                 if (!data.includes("Error")) {
                     var content = JSON.parse(data);
-                    //$("#formularioPadrinosLibres")[0].reset();//limpia el formulario
-                    //tipo,titulo,mensaje
                     //check("success", "OK", "Datos recuperados");
                     var n = content.length;
 
@@ -59,8 +57,6 @@ function loadTablaPadrino(){
                         tds += '<td>' + content[i].nombre + '</td>';
                         tds += '<td>' + content[i].apellido + '</td>';
                         tds += '<td>' + content[i].dni + '</td>';
-                        /* tds += '<td>' + content[i].contacto + '</td>';*/
-                        tds += '<td>' + content[i].fechaAlta.split(" ")[0] + '</td>';
                         tds += '<td>' + ' ' + '</td>';
                         tds += '<td><button type="button" class="btn" id="myBtn" data-toggle="modal" data-target="#Ahijado" data-show="true" onclick = "mostrarModalAlumnos(' + (i + 1) + ')">asignar</button></td>';
                         tds += '</tr>';
@@ -75,8 +71,6 @@ function loadTablaPadrino(){
                     //tipo,titulo,mensaje
                     check("error", "Error al cargar los datos", data);
                 }
-
-                //$("#respuesta").html(data); // Mostrar la respuestas del script PHP.
 
             }
         });
@@ -96,9 +90,8 @@ function mostrarModalAlumnos(id) {
     var apodo = rows[id].getElementsByTagName("TD")[0];
     var nombre = rows[id].getElementsByTagName("TD")[1];
     var apellido = rows[id].getElementsByTagName("TD")[2];
-    //check("info", "", apodo.innerHTML + " " + nombre.innerHTML + " " + apellido.innerHTML);
 
-    $("#apadrinarAlumno").html("Padrino elegido: " + nombre.innerHTML + " " + apellido.innerHTML); // Mostrar la respuestas del script PHP.
+    $("#apadrinarAlumno").html("Padrino elegido: <b>" + nombre.innerHTML + " " + apellido.innerHTML+"</b>"); // Mostrar la respuestas del script PHP.
 
     var url = "../view/consultarDatos.php"; // El script a dónde se realizará la petición.
     $.ajax({
@@ -116,10 +109,9 @@ function mostrarModalAlumnos(id) {
                 for (var i = 0; i < n; i++) {
                     var alumno = content[i].nombre + " " + content[i].apellido;
                     tds = '<tr>';
-                    //tds += '<td contenteditable="true"><input type="radio" value="' + alumno + '" checked> ' + alumno + '<br></td>';
-                   // tds += '<td><label for="' + content[i].id + '"><input type="checkbox" name="' + content[i].id + '" id="' + content[i].id + '" value="' + alumno + '" />' + alumno + '</label></td>';
-                    tds +='<td><input type="radio" name="radioAlumno" value="' + content[i].id + '"></td>';
-                    tds +='<td>' + alumno + '</td>';
+                    //en el value se colocan los id del alumno y el padrino, para poder asociarlos
+                    tds +='<td><input type="radio" name="radioAlumno" value="'+alumno+"*" + content[i].id+"*"+id+ '">' + alumno + '</td>';
+                  //  tds +='<td>' + alumno + '</td>';
                     tds += '</tr>';
                     $("#tablaAlumnosLibres").append(tds);
                 }
@@ -129,7 +121,6 @@ function mostrarModalAlumnos(id) {
                 check("error", "Error al cargar los datos", data);
             }
 
-            //$("#respuesta").html(data); // Mostrar la respuestas del script PHP.
 
         }
     });
@@ -139,6 +130,12 @@ function mostrarModalAlumnos(id) {
  * Cuando se guarda la asociacion entre padrino y alumno
  */
 function guardarApadrinaje() {
+    var select=document.querySelector('input[name = "radioAlumno"]:checked').value;
+    check("info", "OK", select.split("*")[0]+" "+select.split("*")[1]+" "+select.split("*")[2]);
+    var table = document.getElementById("tablaPadrinos");
+    var rows = table.getElementsByTagName("TR");
+    rows[select.split("*")[2]].getElementsByTagName("TD")[4].innerHTML=select.split("*")[0];
+
     cancelarModalAlumnos();
 }
 /**
@@ -158,9 +155,63 @@ function cancelarModalAlumnos() {
  * Para que la tabla de Padrinos libres se ordene
  * @param {[[Type]]} n [[Description]]
  */
-function sortTable(n) {
+function ordenarTablaPadrinosLibres(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
     table = document.getElementById("tablaPadrinos");
+    switching = true;
+    //Set the sorting direction to ascending:
+    dir = "asc";
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.getElementsByTagName("TR");
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 1; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            /*check if the two rows should switch place,
+            based on the direction, asc or desc:*/
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            //Each time a switch is done, increase this count by 1:
+            switchcount++;
+        } else {
+            /*If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again.*/
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+}
+function ordenarTablaAlumnosLibres(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("tablaAlumnosLibres");
     switching = true;
     //Set the sorting direction to ascending:
     dir = "asc";
