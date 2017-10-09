b16798c4c6c49738e81bf251f3936792367ee85f
Santiago <Santiago@DESKTOP-0JATFB5>
2017-10-08 21:31:32 -0300

    Mensaje de alerta

--- resource/js/scriptCarga.js
+++ resource/js/scriptCarga.js
@@ -3,69 +3,96 @@
  * @returns
  */
 $(function () {
- $("#cargaAlumno").click(function () {
- var url = "../view/cargarDatos.php"; // El script a dónde se realizará la petición.
-    $.ajax({
-           type: "POST",
-           url: url,
-           data: $("#formularioAlumno").serialize()+"&tipo=Alumno", // Adjuntar los campos del formulario enviado.
-           success: function(data)
-           {
-               if(data.includes("correctamente")){
-                        $("#formularioAlumno")[0].reset();//limpia el formulario
-                        //tipo,titulo,mensaje
-                       check("success","OK",data);
-                    }else{
-                        //tipo,titulo,mensaje
-                        check("error","Error al cargar los datos",data);
-                    }
+    $("#cargaAlumno").click(function () {
+        var url = "../view/cargarDatos.php"; // El script a dónde se realizará la petición.
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: $("#formularioAlumno").serialize() + "&tipo=Alumno", // Adjuntar los campos del formulario enviado.
+            success: function (data)
+            {
+                if (data.includes("correctamente")) {
+                    $("#formularioAlumno")[0].reset();//limpia el formulario
+                    //tipo,titulo,mensaje
+                    check("success", "OK", data);
+                } else {
+                    //tipo,titulo,mensaje
+                    check("error", "Error al cargar los datos", data);
+                }
 
-	               //$("#respuesta").html(data); // Mostrar la respuestas del script PHP.
+                //$("#respuesta").html(data); // Mostrar la respuestas del script PHP.
 
-           }
-         }); 
+            }
+        });
 
-    return false; // Evitar ejecutar el submit del formulario.
- });
+        return false; // Evitar ejecutar el submit del formulario.
+    });
 });
 /**
  * Para cargar un padrino
  * @returns 
- */ 
-$(function(){
-	 $("#cargaPadrino").click(function(){
-	 var url = "../view/cargarDatos.php"; // El script a dónde se realizará la petición.
-	    $.ajax({
-	           type: "POST",
-	           url: url,
-	           data: $("#formularioPadrino").serialize()+"&tipo=Padrino", // Adjuntar los campos del formulario enviado.
-	           success: function(data)
-	           {
-                   if(data.includes("correctamente")){
-                        $("#formularioPadrino")[0].reset();//limpia el formulario
-                        //tipo,titulo,mensaje
-                       check("success","OK",data);
-                    }else{
-                        //tipo,titulo,mensaje
-                        check("error","Error al cargar los datos",data);
-                    }
+ */
+$(function () {
+    $("#cargaPadrino").click(function () {        
+        var url = "../view/cargarDatos.php"; // El script a dónde se realizará la petición.
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: $("#formularioPadrino").serialize() + "&tipo=Padrino", // Adjuntar los campos del formulario enviado.
+            success: function (data)
+            {
+                if (data.includes("correctamente")) {
+                    $("#formularioPadrino")[0].reset();//limpia el formulario
+                    //tipo,titulo,mensaje
+                    check("success", "OK", data);
+                } else {
+                    //tipo,titulo,mensaje
+                    check("error", "Error al cargar los datos", data);
+                }
 
-	               //$("#respuesta").html(data); // Mostrar la respuestas del script PHP.
+                //$("#respuesta").html(data); // Mostrar la respuestas del script PHP.
 
-	           }
-	         });
+            }
+        });
 
-	    return false; // Evitar ejecutar el submit del formulario.
-	 });
-	});
+        return false; // Evitar ejecutar el submit del formulario.
+    });
+});
+
+$(function () {
+    $("#cambioClave").click(function () {
+        var url = "../view/cargarDatos.php"; // El script a dónde se realizará la petición.
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: $("#formularioCambioClave").serialize() + "&tipo=CambioClave", // Adjuntar los campos del formulario enviado.
+            success: function (data)
+            {
+                if (data.includes("correctamente")) {
+                    $("#formularioCambioClave")[0].reset();//limpia el formulario
+                    //tipo,titulo,mensaje
+                    check("success", "OK", data);
+                } else {
+                    //tipo,titulo,mensaje
+                    check("error", "Error", data);
+                }
+
+                //$("#respuesta").html(data); // Mostrar la respuestas del script PHP.
+
+            }
+        });
+
+        return false; // Evitar ejecutar el submit del formulario.
+    });
+});
 
 
 //Modal *******************************************************************
 $(function () {
- $("#pruebaAlerta").click(function () {
+    $("#pruebaAlerta").click(function () {
 
-            check(document.getElementById('name').value,"titulo","mensaje");  // $("#respuesta").html("Esta es una prueba de gabriel"); // Mostrar la respuestas del script PHP.
-              //  $("#myModal").modal();
+        check(document.getElementById('name').value, "titulo", "mensaje");  // $("#respuesta").html("Esta es una prueba de gabriel"); // Mostrar la respuestas del script PHP.
+        //  $("#myModal").modal();
 
- });
+    });
 });
